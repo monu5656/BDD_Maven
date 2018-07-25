dae650e001a5ddf6435aadb029ec614e97b14bdb
IBM <IBM@DEVOPS_06>
2018-07-25 08:56:08 +0530

    update

--- BDD2/src/test/java/StepDefinition/StepDefinition.java
+++ BDD2/src/test/java/StepDefinition/StepDefinition.java
@@ -3,9 +3,6 @@ package StepDefinition;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
